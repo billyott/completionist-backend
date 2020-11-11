adae414bfe79e3e2c4c1f4023fb7e605bab0f693
bfeldman <brian.feldman42@gmail.com>
2020-11-11 18:27:12 -0500

    tweaks

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,4 +1,4 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :title, :priority_level, :due_date, :tag, :description, :completion_status
+  attributes :id, :title, :priority_level, :due_date, :tag, :description, :completion_status, :user_id
   has_many :subtasks
 end
